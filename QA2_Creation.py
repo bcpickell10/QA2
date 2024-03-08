7b098d6c5410e102df9615aba1304a9cc4c17f0e
YourName <youremail@website.com>
2024-03-08 08:45:17 -0600

    Added notes to create file

--- QA2_Creation.py
+++ QA2_Creation.py
@@ -2,22 +2,27 @@ import sqlite3 as sql
 conn=sql.connect('quarterly_assessment.db')
 cr=conn.cursor()
 
+# Create Table 1
 cr.execute(
     """CREATE TABLE IF NOT EXISTS Finance
     (Finance_Question TEXT PRIMARY KEY, Answer TEXT)""")
 
+# Create Table 2
 cr.execute(
     """CREATE TABLE IF NOT EXISTS Analytics
     (Analytics_Question TEXT PRIMARY KEY, Answer TEXT)""")
 
+# Create Table 3
 cr.execute(
     """CREATE TABLE IF NOT EXISTS Business_Management
     (BMGT_Question TEXT PRIMARY KEY, Answer TEXT)""")
 
+# Create Table 4
 cr.execute(
     """CREATE TABLE IF NOT EXISTS Analytical_Thinking
     (AT_Question TEXT PRIMARY KEY, Answer TEXT)""")
 
+# Create Table 5
 cr.execute(
     """CREATE TABLE IF NOT EXISTS Apps_Development
     (AD_Question TEXT PRIMARY KEY, Answer TEXT)""")
